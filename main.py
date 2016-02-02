cb981671be9c9a084cd9b1f8d621c56dd5d60c3a
sarp izgi <sarp_izgi@yahoo.com.tr>
2016-02-02 03:09:17 +0200

    final

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-analiste=
+import random
+import string
+import collections
+
+analiste=[]
+
+howmanywords=500000
+
+
+def createword():
+    for i in range(howmanywords):
+        X=random.randint(2,8)
+        word=''.join(random.choice(string.ascii_lowercase) for _ in range(X))
+        analiste.append(word)
+    print(analiste)
+
+
+
+
+createword()
+print(collections.Counter(analiste).most_common())
+collections.Counter
